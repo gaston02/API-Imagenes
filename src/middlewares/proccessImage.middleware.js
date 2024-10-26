e8cf9eafe37981745e6e885f37a6ec340bc16c28
Gaston Fuentes <gastonandres.fuentes@gmail.com>
2024-10-26 19:10:24 -0300

    fix proccessImageMiddleware

--- src/middlewares/proccessImage.middleware.js
+++ src/middlewares/proccessImage.middleware.js
@@ -6,12 +6,10 @@ export const processImage = async (req, res, next) => {
     const outputPath = inputPath;
 
     await compressImage(inputPath, outputPath); // Llama a la función para comprimir la imagen
-    res
-      .status(200)
-      .json({
-        message: "Imagen subida y procesada con éxito.",
-        imagePath: outputPath,
-      });
+    res.status(200).json({
+      message: "Imagen subida y procesada con éxito.",
+      imagePath: outputPath,
+    });
   } catch (error) {
     next(error); // Pasar el error al siguiente middleware de manejo de errores
   }
